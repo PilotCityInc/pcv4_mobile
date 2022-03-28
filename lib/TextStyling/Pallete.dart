0256ff5045008b09a348399ce428fc5280e34acb
Prajit Saravanan <Prajit.saravana@gmail.com>
2022-03-28 12:11:09 -0700

    contains the text styling for universal usage

---
+++ lib/TextStyling/Pallete.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unnecessary_import, constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+const TextStyle LoginPageHeading = TextStyle(
+    decoration: TextDecoration.none,
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    color: Colors.blue);
