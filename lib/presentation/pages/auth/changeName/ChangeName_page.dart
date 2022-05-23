aa8e9441af6355678553896a53094c982994e21d
Prajit Saravanan <Prajit.saravana@gmail.com>
2022-05-23 01:50:58 -0700

    edited change

--- lib/presentation/pages/auth/changeName/ChangeName_page.dart
+++ lib/presentation/pages/auth/changeName/ChangeName_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'widgets/chnageName_form.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+class ChangeNamePage extends StatelessWidget {
+  const ChangeNamePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
