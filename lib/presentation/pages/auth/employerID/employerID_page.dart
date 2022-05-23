c381d5194352fe86ee44a22660ea0a7bd722cf34
Prajit Saravanan <Prajit.saravana@gmail.com>
2022-05-23 01:29:30 -0700

    Adeed the Folder Structer

--- lib/presentation/pages/auth/employerID/employerID_page.dart
+++ lib/presentation/pages/auth/employerID/employerID_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'widgets/chnageName_form.dart';
+import 'widgets/employerID_form.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+class EmployerIDPage extends StatelessWidget {
+  const EmployerIDPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Color(0xFF404142),
-      body: ChangeNameForm(),
+      body: EmployerIDForm(),
     );
   }
 }
