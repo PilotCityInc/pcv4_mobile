8268330700c28d44478736d7cfa4c4534cd96343
Prajit Saravanan <Prajit.saravana@gmail.com>
2022-05-24 19:56:23 -0700

    Added the Folder structer

--- lib/presentation/pages/auth/schoolID/schoolID_page.dart
+++ lib/presentation/pages/auth/schoolID/schoolID_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'widgets/chnageName_form.dart';
+import 'widgets/schoolID_form.dart';
 
-class ChangeNamePage extends StatelessWidget {
-  const ChangeNamePage({Key? key}) : super(key: key);
+class SchoolIDPage extends StatelessWidget {
+  const SchoolIDPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Color(0xFF404142),
-      body: ChangeNameForm(),
+      body: SchoolIDForm(),
     );
   }
 }
